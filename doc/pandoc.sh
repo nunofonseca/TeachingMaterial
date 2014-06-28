5bbb16d02395b100e98f09ac692c1056f458ac0b
M.P. Barzine <barzine@gmail.com>
2014-06-28 14:08:17 +0100

    Update pandoc.sh

--- doc/pandoc.sh
+++ doc/pandoc.sh
@@ -13,4 +13,4 @@
 ##          --variable urlcolor:black 
 ##          --variable geometry:margin=3cm
 
-pandoc *.md -o ../pdf/practical.pdf --toc --variable title:"RNA-seq data analysis practical - Cancer Genomics Workshop" --variable date:"2014/07/03" --variable author:"Mar Gonzàlez-Porta - Mitra P. Barzine - Liliana Greger" --variable links-as-notes --variable linkcolor:black --variable urlcolor:black --variable geometry:margin=3cm
+pandoc *.md -o ../pdf/practical.pdf --toc --variable title:"RNA-seq data analysis practical - Cancer Genomics Workshop" --variable date:"2014/07/03" --variable author:"Mitra P. Barzine and Liliana Greger" --variable links-as-notes --variable linkcolor:black --variable urlcolor:black --variable geometry:margin=3cm
